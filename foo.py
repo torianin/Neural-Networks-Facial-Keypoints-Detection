858fee523f18f44054ee563a5d50bf5910312a18
Filip Pacanowski <fpacanowski@gmail.com>
2015-02-15 20:47:01 +0100

    cleanup

--- foo.py
+++ foo.py
@@ -14,6 +14,8 @@ from utils import load_model, store_model
 
 pyplot = plt
 
+CUDA = False
+
 def show_plot():
     fig = plt.figure(1)
     fig.set_size_inches(10,10)
@@ -33,9 +35,9 @@ def load_data():
     y = data[data.columns[:-1]].values
     y = (y - 48) / 48
     y = y.astype(np.float32)
-    p = np.random.permutation(len(X))
-    X = X[p]
-    y = y[p]
+    #p = np.random.permutation(len(X))
+    #X = X[p]
+    #y = y[p]
     return X, y
 
 
@@ -44,6 +46,7 @@ def train_network(X, y):
         layers=[  # three layers: one hidden layer
             ('input', layers.InputLayer),
             ('hidden', layers.DenseLayer),
+            ('dropout', layers.DropoutLayer),
             ('output', layers.DenseLayer),
             ],
         # layer parameters:
@@ -51,6 +54,7 @@ def train_network(X, y):
         hidden_num_units=100,  # number of units in hidden layer
         output_nonlinearity=None,  # output layer uses identity function
         output_num_units=30,  # 30 target values
+        dropout_p=0.5,
     
         # optimization method:
         update=nesterov_momentum,
@@ -58,15 +62,19 @@ def train_network(X, y):
         update_momentum=0.9,
     
         regression=True,  # flag to indicate we're dealing with regression problem
-        max_epochs=400,  # we want to train this many epochs
+        max_epochs=3000,  # we want to train this many epochs
         verbose=1,
         )
     net1.fit(X, y)
     return net1
 
 def train_network2(X, y):
-    Conv2DLayer = layers.Conv2DLayer
-    MaxPool2DLayer = layers.MaxPool2DLayer
+    if CUDA:
+        Conv2DLayer = layers.cuda_convnet.Conv2DCCLayer
+        MaxPool2DLayer = layers.cuda_convnet.MaxPool2DCCLayer
+    else:
+        Conv2DLayer = layers.Conv2DLayer
+        MaxPool2DLayer = layers.MaxPool2DLayer
     
     net2 = NeuralNet(
         layers=[
@@ -78,6 +86,7 @@ def train_network2(X, y):
             ('conv3', Conv2DLayer),
             ('pool3', MaxPool2DLayer),
             ('hidden4', layers.DenseLayer),
+            ('dropout', layers.DropoutLayer),
             ('hidden5', layers.DenseLayer),
             ('output', layers.DenseLayer),
             ],
@@ -86,6 +95,7 @@ def train_network2(X, y):
         conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_ds=(2, 2),
         conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_ds=(2, 2),
         hidden4_num_units=500, hidden5_num_units=500,
+        dropout_p=0.5,
         output_num_units=30, output_nonlinearity=None,
     
         update_learning_rate=0.01,
@@ -95,46 +105,11 @@ def train_network2(X, y):
         max_epochs=10,
         verbose=1,
         )
+    net2.fit(X.reshape(-1, 1, 96, 96), y)
     return net2
 
 
-# use the cuda-convnet implementations of conv and max-pool layer
-#Conv2DLayer = layers.cuda_convnet.Conv2DCCLayer
-#MaxPool2DLayer = layers.cuda_convnet.MaxPool2DCCLayer
-
-
-#X, y = load_data()
+X, y = load_data()
+net2 = train_network2(X, y)
 #store_model([X, y], 'data.pickle')
-#X, y = load2d()  # load 2-d data
-#net2.fit(X.reshape(-1, 1, 96, 96), y)
-#store_model(net2, 'conv_net.pickle')
-
-X, y = load_model('data.pickle')
-net2 = load_model('conv_net.pickle')
-
-#testX = X[:100]
-#X = X[100:]
-#y = y[100:]
-#net = train_network(X, y)
-#store_model(net, 'simple_net.pickle')
-#net1 = load_model('simple_net.pickle')
-def plot_samples(net, X):
-    def plot_sample(x, y, axis):
-        img = x.reshape(96, 96)
-        axis.imshow(img, cmap='gray')
-        axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
-    
-    y_pred = net.predict(X)
-    
-    fig = pyplot.figure(figsize=(6, 6))
-    fig.subplots_adjust(
-        left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
-    
-    for i in range(16):
-        ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
-        plot_sample(X[i], y_pred[i], ax)
-    
-    show_plot()
-
-X = X[:100].reshape(-1, 1, 96, 96)
-plot_samples(net2, X)
+store_model(net2, 'conv_net.pickle')
